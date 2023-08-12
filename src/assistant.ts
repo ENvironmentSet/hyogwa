8595b9280ad92bd8097772b725735881f2f4a0a8
ENvironmentSet <sbshw1@gmail.com>
2023-08-12 11:55:57 +0900

    re-format 'InspectEffectHandling'

--- src/assistant.ts
+++ src/assistant.ts
@@ -20,46 +20,46 @@ export type InspectEffectfulFunction<G extends (...args: never) => Generator>
         >
       : Suggestion<'Fail to extract type information from the given function.'>
 
-export type InspectEffectHandling<C extends Effectful<Spec, unknown>, H> =
-  C extends Effectful<infer SS, infer R> ?
-    string extends (SS extends Spec<infer N> ? N : never) ?
-      Suggestion<'Malformed effect specification is found. '>
-      : H extends Partial<Handlers<SS, R>> ?
-        Suggestion<
-          'With the given handler, the computation can be handled while resulting following:',
-          {
-            'effects will be resolved by handling': keyof H
-            'effects will remain after handling': Exclude<SS, { [EFFECT_NAME]: keyof H }> |  CollectEffectsFromHandlers<H>
-            'result of handling': R
-          }
-        >
-        : Handlers<SS, R> extends infer OH ?
-          keyof OH extends infer ENS ?
-            ENS extends infer EN extends keyof OH ?
-              EN extends keyof H ?
-                keyof OH[EN] extends infer ANS ?
-                  ANS extends infer AN extends keyof OH[EN] ?
-                    AN extends keyof H[EN] ?
-                      H[EN][AN] extends OH[EN][AN] ?
-                        never
-                        : Suggestion<
-                          'Type of action handler possibly wrong',
-                          {
-                            'expected type': OH[EN][AN]
-                            'given type': H[EN][AN]
-                          }
-                        >
-                      : Suggestion<
-                        'Absence of required action handler is found',
-                        {
-                          'name of missing handler': AN,
-                          'type of missing handler': OH[EN][AN]
-                        }
-                      >
+export type InspectEffectHandling<C extends Effectful<Spec, unknown>, H>
+  = C extends Effectful<infer SS, infer R> ?
+      string extends (SS extends Spec<infer N> ? N : never) ?
+        Suggestion<'Malformed effect specification is found. '>
+        : H extends Partial<Handlers<SS, R>> ?
+            Suggestion<
+              'With the given handler, the computation can be handled while resulting following:',
+              {
+                'effects will be resolved by handling': keyof H
+                'effects will remain after handling': Exclude<SS, { [EFFECT_NAME]: keyof H }> |  CollectEffectsFromHandlers<H>
+                'result of handling': R
+              }
+            >
+            : Handlers<SS, R> extends infer OH ?
+              keyof OH extends infer ENS ?
+                ENS extends infer EN extends keyof OH ?
+                  EN extends keyof H ?
+                    keyof OH[EN] extends infer ANS ?
+                      ANS extends infer AN extends keyof OH[EN] ?
+                        AN extends keyof H[EN] ?
+                          H[EN][AN] extends OH[EN][AN] ?
+                            never
+                            : Suggestion<
+                              'Type of action handler possibly wrong',
+                              {
+                                'expected type': OH[EN][AN]
+                                'given type': H[EN][AN]
+                             }
+                            >
+                          : Suggestion<
+                            'Absence of required action handler is found',
+                            {
+                              'name of missing handler': AN,
+                              'type of missing handler': OH[EN][AN]
+                            }
+                          >
+                        : never
+                      : never
                     : never
                   : never
                 : never
-              : never
             : never
-          : never
     : Suggestion<'Fail to extract effect specifications or result type from the given effectful computation.'>
