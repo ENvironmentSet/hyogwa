9d75b29a7d52eedfcec44a7fd89ab57203a1fb4b
ENvironmentSet <sbshw1@gmail.com>
2023-08-12 11:52:53 +0900

    re-format 'InspectEffectfulFunction'

--- src/assistant.ts
+++ src/assistant.ts
@@ -2,23 +2,23 @@ import { Spec, Action, Effectful, Handlers, EFFECT_NAME, CollectEffectsFromHandl
 
 type Suggestion<M extends string, T = string> = M & T
 
-export type InspectEffectfulFunction<G extends (...args: never) => Generator> =
- G extends (...args: never) => Generator<infer AS, infer R> ?
-    [AS] extends [Action<string, string, unknown[]>] ?
-      Suggestion<
-        'Effects used in the function and return type of the function is inferred as following. Consider typing the function properly with this information.',
-        {
-          'names of effect used in the function': AS extends Action<infer N, string, unknown[]> ? N : never
-          'return type of the function': R
-        }
-      >
-      : Suggestion<
-        'Effectful functions must be generator functions which only yield actions but the given function yields non-action value.',
-        {
-          'types of non-action value': Exclude<AS, Action<string, string, unknown[]>>
-        }
-      >
-    : Suggestion<'Fail to extract type information from the given function.'>
+export type InspectEffectfulFunction<G extends (...args: never) => Generator>
+  = G extends (...args: never) => Generator<infer AS, infer R> ?
+      [AS] extends [Action<string, string, unknown[]>] ?
+        Suggestion<
+          'Effects used in the function and return type of the function is inferred as following. Consider typing the function properly with this information.',
+          {
+            'names of effect used in the function': AS extends Action<infer N, string, unknown[]> ? N : never
+            'return type of the function': R
+          }
+        >
+        : Suggestion<
+          'Effectful functions must be generator functions which only yield actions but the given function yields non-action value.',
+          {
+            'types of non-action value': Exclude<AS, Action<string, string, unknown[]>>
+          }
+        >
+      : Suggestion<'Fail to extract type information from the given function.'>
 
 export type InspectEffectHandling<C extends Effectful<Spec, unknown>, H> =
   C extends Effectful<infer SS, infer R> ?
