9bfa3a960a8747f4670ee9d4dfb497539d4a8592
ENvironmentSet <sbshw1@gmail.com>
2023-08-25 20:37:11 +0900

    Add `withHandler`

---
+++ src/with-handler.ts
@@ -0,0 +1,21 @@
+import { Effectful, Effects, ExcludeHandledEffects, handle, Handlers, UsedEffectsInHandlers } from './core'
+
+/**
+ * Wraps function with given handlers
+ *
+ * Handles effects of given effectful function
+ *
+ * @beta
+ *
+ * @param f - An effectful function to be handled
+ * @param handlers - A handlers to handle effects of `f`
+ */
+export function withHandler
+  <P extends unknown[], E extends Effects, R, H extends Partial<Handlers<E, R>>> // Type parameters
+  (f: (...parameters: P) => Effectful<E, R>, handlers: H) // Parameters
+  : (...parameters: P) => Effectful<ExcludeHandledEffects<E, H> | UsedEffectsInHandlers<H>, R> { // Return type
+  return (...parameters) => handle(
+    f(...parameters),
+    handlers
+  )
+}
